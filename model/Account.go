4fd688b5753b485cb5183cd5fb6959802cb8cdc8
pooulad <poooooladi@gmail.com>
2023-11-02 20:52:43 +0330

    add json value for send in json encdoder

--- model/Account.go
+++ model/Account.go
@@ -2,20 +2,19 @@ package model
 
 import "math/rand"
 
-
 type Account struct {
-	ID        int
-	FirstName string
-	LastName  string
-	Number    int64
-	Balance   int64
+	ID        int    `json:"id"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Number    int64  `json:"number"`
+	Balance   int64  `json:"balance"`
 }
 
 func NewAccount(firstName string, lastName string) *Account {
 	return &Account{
-		ID: rand.Intn(10000),
+		ID:        rand.Intn(10000),
 		FirstName: firstName,
 		LastName:  lastName,
-		Number: int64(rand.Intn(1000000)),
+		Number:    int64(rand.Intn(1000000)),
 	}
 }
