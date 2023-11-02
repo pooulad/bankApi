60b5b44dd854f20e9df797afba64fc975c01b3a9
pooulad <poooooladi@gmail.com>
2023-11-02 19:29:34 +0330

    create WriteJson func

--- api/http.go
+++ api/http.go
@@ -4,3 +4,9 @@ type ApiError struct {
 	Error string
 }
 
+func WriteJson(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-type", "application/json")
+	return json.NewEncoder(w).Encode(w)
+}
+
