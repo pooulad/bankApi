4fb2f74c665488b81e67818731bbcea7a22f21b2
pooulad <poooooladi@gmail.com>
2023-11-03 00:46:09 +0330

    create PostgresConfig struct

---
+++ config/config.go
@@ -0,0 +1,8 @@
+type PostgresConfig struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Host     string `json:"host"`
+	Name     string `json:"name"`
+	SSL      string `json:"ssl"`
+}
+
