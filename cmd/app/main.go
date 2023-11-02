9c4a7ce40835f846547b9ce2d32985957c840428
pooulad <poooooladi@gmail.com>
2023-11-02 19:58:55 +0330

    read env file and extract data

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,4 +4,12 @@ import "fmt"
 
 func main(){
 	fmt.Println("run now")
-}
+}	if err != nil {
+		log.Fatalf("Some error occured. Err: %s", err)
+	}
+
+	host := os.Getenv("PROJECT_HOST")
+	port := os.Getenv("PROJECT_PORT")
+
+	serverAddress := fmt.Sprintf("%s:%s", host, port)
+
