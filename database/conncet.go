4bb8054ae5f9287cfe91c378efa5ffc983183920
pooulad <poooooladi@gmail.com>
2023-11-03 00:45:50 +0330

    create ConnectDB func

--- database/conncet.go
+++ database/conncet.go
@@ -1,3 +1,14 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+	"github.com/pooulad/bankApi/config"
+	"github.com/pooulad/bankApi/model"
+)
+
 type Storage interface {
 	CreateAccount(*model.Account) error
 	DeleteAccount(int) error
@@ -9,3 +20,20 @@ type PostgresStore struct {
 	db *sql.DB
 }
 
+func ConnectDB(cfg *config.PostgresConfig) (*PostgresStore, error) {
+	dsn := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=%s", cfg.Username, cfg.Password, cfg.Host, cfg.Name, cfg.SSL)
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		fmt.Println("postgres connected")
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	return &PostgresStore{
+		db: db,
+	}, nil
+}
