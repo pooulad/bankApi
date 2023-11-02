5162c9c9eec7b6d7e44927aee02bd839a704de19
pooulad <poooooladi@gmail.com>
2023-11-03 00:46:20 +0330

    create ReadPostgresConfig func

--- config/config.go
+++ config/config.go
@@ -1,3 +1,11 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
 type PostgresConfig struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
@@ -6,3 +14,21 @@ type PostgresConfig struct {
 	SSL      string `json:"ssl"`
 }
 
+func ReadPostgresConfig(path string) (*PostgresConfig, error) {
+	if path == "" {
+		return nil, fmt.Errorf("no path provided")
+	}
+
+	file, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg PostgresConfig
+	err = json.Unmarshal(file, &cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
