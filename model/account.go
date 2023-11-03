e8e474384e9f91eac11d19a24b270a23b4883438
pooulad <poooooladi@gmail.com>
2023-11-03 03:45:44 +0330

    Update account.go

--- model/account.go
+++ model/account.go
@@ -8,8 +8,14 @@ import (
 type CreateAccountRequest struct {
 	FirstName string `json:"firstName"`
 	LastName  string `json:"lastName"`
-	Number    int64  `json:"number"`
-	Balance   int64  `json:"balance"`
+}
+type Account struct {
+	ID        int       `json:"id"`
+	FirstName string    `json:"firstName"`
+	LastName  string    `json:"lastName"`
+	Number    int64     `json:"number"`
+	Balance   int64     `json:"balance"`
+	CreatedAt time.Time `json:"createdAt"`
 }
 
 func NewAccount(firstName string, lastName string) *Account {
@@ -18,5 +24,6 @@ func NewAccount(firstName string, lastName string) *Account {
 		FirstName: firstName,
 		LastName:  lastName,
 		Number:    int64(rand.Intn(1000000)),
+		CreatedAt: time.Time.UTC(),
 	}
 }
