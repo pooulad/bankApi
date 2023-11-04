7ae866db609d14898e411056c0f526ced405802f
pooulad <poooooladi@gmail.com>
2023-11-04 15:35:38 +0330

    write new query for delete account

--- database/repository.go
+++ database/repository.go
@@ -52,7 +52,8 @@ func (s *PostgresStore) UpdateAccount(*model.Account) error {
 }
 
 func (s *PostgresStore) DeleteAccount(id int) error {
-	return nil
+	_, err := s.db.Query("DELETE FROM account WHERE id = $1", id)
+	return err
 }
 
 func (s *PostgresStore) GetAccountById(id int) (*model.Account, error) {
