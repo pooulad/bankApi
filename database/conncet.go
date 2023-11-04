256b728e2c3fa26c33778624007f6e257e56e452
pooulad <poooooladi@gmail.com>
2023-11-04 14:27:09 +0330

    move all storafe functions in new file in database directory

--- database/conncet.go
+++ database/conncet.go
@@ -6,16 +6,7 @@ import (
 
 	_ "github.com/lib/pq"
 	"github.com/pooulad/bankApi/config"
-	"github.com/pooulad/bankApi/model"
 )
-
-type Storage interface {
-	GetAccounts() ([]*model.Account, error)
-	CreateAccount(*model.Account) error
-	DeleteAccount(int) error
-	UpdateAccount(*model.Account) error
-	GetAccountById(int) error
-}
 
 type PostgresStore struct {
 	db *sql.DB
@@ -38,78 +29,4 @@ func ConnectDB(cfg *config.PostgresConfig) (*PostgresStore, error) {
 	return &PostgresStore{
 		db: db,
 	}, nil
-}
-
-func (s *PostgresStore) Init() error {
-	return s.CreateAccountTable()
-}
-
-func (s *PostgresStore) CreateAccountTable() error {
-	query := `create table if not exists account (
-		id serial primary key,
-		first_name varchar(50),
-		last_name varchar(50),
-		number serial,
-		balance serial,
-		created_at timestamp
-	)`
-	_, err := s.db.Exec(query)
-	return err
-}
-
-func (s *PostgresStore) CreateAccount(acc *model.Account) error {
-	query := `insert into account
-		(first_name,last_name,number,balance,created_at)
-		values ($1,$2,$3,$4,$5)`
-
-	resp, err := s.db.Query(query, acc.FirstName, acc.LastName, acc.Number, acc.Balance, acc.CreatedAt)
-	if err != nil {
-		return err
-	}
-
-	fmt.Printf("%+v\n", resp)
-
-	return nil
-}
-
-func (s *PostgresStore) UpdateAccount(*model.Account) error {
-	return nil
-}
-
-func (s *PostgresStore) DeleteAccount(id int) error {
-	return nil
-}
-
-func (s *PostgresStore) GetAccountById(id int) error {
-	return nil
-}
-
-func (s *PostgresStore) GetAccounts() ([]*model.Account, error) {
-	rows, err := s.db.Query("SELECT * FROM account")
-	if err != nil {
-		return nil, err
-	}
-
-	defer rows.Close()
-
-	accounts := []*model.Account{}
-
-	for rows.Next() {
-		account := new(model.Account)
-		err := rows.Scan(&account.ID, &account.FirstName, &account.LastName,
-			&account.Number, &account.Balance, &account.CreatedAt)
-
-		if err != nil {
-			return nil, err
-		}
-
-		accounts = append(accounts, account)
-	}
-
-	if err = rows.Err(); err != nil {
-		return accounts, err
-	}
-
-	return accounts, nil
-
 }
