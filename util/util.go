c0d69a2c297ea043d9f78d229d7386b72af868ee
pooulad <poooooladi@gmail.com>
2023-11-04 15:34:46 +0330

    create new func for check id in url

--- util/util.go
+++ util/util.go
@@ -2,7 +2,11 @@ package util
 
 import (
 	"database/sql"
+	"fmt"
+	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/pooulad/bankApi/model"
 )
 
@@ -12,4 +16,15 @@ func ScanSqlRows(rows *sql.Rows) (*model.Account, error) {
 		&account.Number, &account.Balance, &account.CreatedAt)
 
 	return account, err
+}
+
+func GetAccountParameterId(r *http.Request) (int, error) {
+	idStr := mux.Vars(r)["id"]
+
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		return id, fmt.Errorf("invalid id given in url:%d", id)
+	}
+
+	return id, nil
 }
