80be0596df6f13b031d68599b2a9888838d39081
pooulad <poooooladi@gmail.com>
2023-11-04 11:23:08 +0330

    delete id from model and auto generate from database

--- model/account.go
+++ model/account.go
@@ -20,7 +20,6 @@ type Account struct {
 
 func NewAccount(firstName string, lastName string) *Account {
 	return &Account{
-		ID:        rand.Intn(10000),
 		FirstName: firstName,
 		LastName:  lastName,
 		Number:    int64(rand.Intn(1000000)),
