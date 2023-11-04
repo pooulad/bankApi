e1c694044f4705216dc075d064514a4d4633fe9c
pooulad <poooooladi@gmail.com>
2023-11-05 00:03:07 +0330

    Update account.go

--- model/account.go
+++ model/account.go
@@ -31,8 +31,8 @@ type Account struct {
 	ID                int       `json:"id"`
 	FirstName         string    `json:"firstName"`
 	LastName          string    `json:"lastName"`
-	Number            int64     `json:"number"`
 	EncryptedPassword string    `json:"-"`
+	Number            int64     `json:"number"`
 	Balance           int64     `json:"balance"`
 	CreatedAt         time.Time `json:"createdAt"`
 }
